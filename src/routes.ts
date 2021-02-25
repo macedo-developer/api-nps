504e517796e3a7c4f7b7fa367ee16050d89c77ca
macedo-developer <macedorenata.developer@gmail.com>
2021-02-24 21:47:43 -0300

    Creating router survey

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Request, Response, Router } from "express";
+import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
 
 const routes = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
 
 routes.post("/users", userController.create);
+routes.post("/surveys", surveysController.create);
 
 export { routes };
