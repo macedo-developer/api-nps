322e70016063bd65136d8b9f8d4909c6f76773e1
macedo-developer <macedorenata.developer@gmail.com>
2021-02-25 23:59:37 -0300

    Refactoring code

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,4 @@
-import "reflect-metadata";
-import express from "express";
-import "./database";
-import { routes } from "./routes";
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
+import { app } from "./app";
 
 const port = 3003;
 
