ed6631d37a0aecdf4b915d6b12d33bf4c4ff3435
macedo-developer <macedorenata.developer@gmail.com>
2021-02-26 17:28:45 -0300

    Testing

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -15,4 +15,12 @@ describe("Users", () => {
     });
     expect(response.status).toBe(201);
   });
+
+  it("Should not be able to create a user with exist email", async () => {
+    const response = await request(app).post("/users").send({
+      email: "user@example.com",
+      name: "User example",
+    });
+    expect(response.status).toBe(400);
+  });
 });
