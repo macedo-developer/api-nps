292ed99447298f01d98b2b76401ab6dc93896587
macedo-developer <macedorenata.developer@gmail.com>
2021-02-28 01:32:28 -0300

    Adding router SendMail

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
+import { SendMainController } from "./controllers/SendMailController";
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
 
@@ -6,9 +7,13 @@ const routes = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
+const sendMainController = new SendMainController();
 
 routes.post("/users", userController.create);
+
 routes.get("/surveys", surveysController.show);
 routes.post("/surveys", surveysController.create);
+
+routes.post("/sendMail", sendMainController.execute);
 
 export { routes };
